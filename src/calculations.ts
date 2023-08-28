7962f1264b137ce7e6ed862655ca0b20e98de98e
Rishad Omar <rishad@aux.co.za>
2023-08-28 13:26:31 +0200

    feat: Add calculateRepaymentAmount function
    This commit adds a new function, calculateRepaymentAmount, which calculates the repayment amount, total interest, end date, and monthly repayments for a loan. The function takes the loan amount, loan period, and interest rate as parameters and returns an object with the calculated values.

--- src/calculations.ts
+++ src/calculations.ts
@@ -34,16 +34,28 @@ export const makeRepaymentTableEntry = (month: number, balance: number, interest
     }
 };
 
+/**
+ * Calculates the repayment amount, total interest, end date, and monthly repayments for a loan.
+ * @param loanAmount The amount of the loan.
+ * @param loanPeriod The period of the loan in months.
+ * @param interestRate The interest rate of the loan.
+ * @returns An object containing the repayment amount, total interest, end date, and monthly repayments.
+ */
 export const calculateRepaymentAmount = (loanAmount: number, loanPeriod: number, interestRate: number): RepaymentResponse => {
+    // Calculate the monthly interest rate.
     const monthlyInterestRate = interestRate / 100 / 12;
 
+    // Calculate the repayment amount.
     const repaymentAmount = loanAmount * (monthlyInterestRate / (1 - Math.pow(1 + monthlyInterestRate, -loanPeriod)));
 
+    // Calculate the total interest.
     const totalInterest = repaymentAmount * loanPeriod - loanAmount;
 
+    // Calculate the end date.
     const endDate = new Date();
     endDate.setMonth(endDate.getMonth() + loanPeriod);
 
+    // Calculate the monthly repayments.
     const monthlyRepayments: Array<MonthRepayment> = [];
     let currentBalance = loanAmount;
     for (let i = 0; i < loanPeriod; i++) {
@@ -52,6 +64,7 @@ export const calculateRepaymentAmount = (loanAmount: number, loanPeriod: number,
         currentBalance = entry.balance;
     }
 
+    // Return an object containing the repayment amount, total interest, end date, and monthly repayments.
     return {
         repaymentAmount: repaymentAmount,
         totalInterest: totalInterest,
